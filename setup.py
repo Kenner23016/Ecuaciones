99fac0b520102cab3ad4b2070c35ee913f60ea7b
Kenner23016 <nm23016@ues.edu.sv>
2025-04-14 19:39:25 -0600

    Actualizaciones de setup

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-from setuptools import setup, find_packages
-
+from setuptools import setup
 setup(
     name="ECUALI_NOLI",
     version="0.1",
-    packages=find_packages(),
+    packages=['ECUALI_NOLI'],
     description="Paquete para resolver ecuaciones lineales y no lineales",
-    author="Tu Nombre",
-    author_email="tu@email.com",
-    install_requires=["numpy", "scipy"],
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    author="Kenner Melgar",
+    author_email="NM23016@ues.edu.sv",
+    url= 'https://github.com/Kenner23016/Ecuaciones.git'
+    install_requires=["numpy", "scipy", "copy"],
 )
